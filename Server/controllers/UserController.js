bc12c6ca5e3fa00199aee442f36018b6865b1bb7
ArshGupta0 <arshguptaarsh1101@gmail.com>
2025-07-29 23:48:47 +0530

    UserController deugging

--- Server/controllers/UserController.js
+++ Server/controllers/UserController.js
@@ -2,29 +2,39 @@
 //http://localhost:4000/api/user/webhooks
 import {Webhook} from 'svix'
 import userModel from '../models/userModels.js'
-const clerkWebhooks = async(req,res)=>{
-    try{
+const clerkWebhooks = async (req, res) => {
+
+    try {
+
+        // Create a Svix instance with clerk webhook secret.
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        await whook.verify(JSON.stringify(req.body),{
-            "svix-id":req.headers["svix-id"],
+
+        // Verifying Headers
+        await whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix-signature"]
+            "svix-signature": req.headers["svix-signature"],
         })
-        const{data,type}= req.body
-        switch (type){
-            case "user.created":{
-                const userData={
+
+        // Getting Data from request body
+        const { data, type } = req.body
+
+        // Switch Cases for differernt Events
+        switch (type) {
+            case 'user.created': {
+                const userData = {
                     clerkId: data.id,
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     firstName: data.first_name,
                     lastName: data.last_name,
-                    photo: data.image_url
+                    photo: data.image_url,
                 }
                 await userModel.create(userData)
                 res.json({})
                 break;
             }
-            case "user.updated":{
+
+            case 'user.updated': {
                 const userData = {
                     email: data.email_addresses[0].email_address,
                     firstName: data.first_name,
@@ -35,19 +45,21 @@ const clerkWebhooks = async(req,res)=>{
                 res.json({})
                 break;
             }
-            case "user.deleted":{
+
+            case 'user.deleted': {
                 await userModel.findOneAndDelete({ clerkId: data.id })
                 res.json({})
                 break;
-
             }
+
             default:
                 break;
         }
-    } catch(error){
-        console.log(error.message)
-        res.json({success:false,message})
 
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
     }
+
 }
 export {clerkWebhooks};
